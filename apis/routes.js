11abb15b289f2fd235893d4464add9bba4d9d264
Batatia, Hadj <hb149@hw.ac.uk>
2021-09-29 22:55:35 +0400

    added routes

---
+++ apis/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const productController = require('../controllers/productControllerEJS');
+
+//define a router and create routes
+const router = express.Router();
+
+//routes for dynamic processing of products
+//-----------------------------------------------
+//route for listing all products
+router.get('/api/catalog', productController.getCatalog);
+router.get('/api/article/:id', productController.getProductByID);
+
+//export router
+module.exports = router;
