bab4ee120592955d3d84befa713e3d0e52da6938
Batatia, Hadj <hb149@hw.ac.uk>
2021-09-29 22:44:57 +0400

    added product controller

---
+++ controllers/productController.js
@@ -0,0 +1,31 @@
+const getCatalog = (request, response) => {
+    const catalogServices = require('../services/productServices');
+    catalogServices.searchService(function(err, rows) {
+        response.json(rows);
+        response.end();
+    });
+};
+
+const getProductByID = (request, response) => {
+    const catalogServices = require('../services/productServices');
+    let reference = request.params.reference;
+    catalogServices.searchIDService(reference, function(err, rows) {
+        response.json(rows);
+        response.end();
+    });
+};
+
+const getProductsByCategory = (request, response) => {
+    const catalogServices = require('../services/productServices');
+    let reference = request.params.category;
+    catalogServices.searchCategoryService(category, function(err, rows) {
+        response.json(rows);
+        response.end();
+    });
+};
+
+module.exports = {
+    getCatalog,
+    getProductByID,
+    getProductsByCategory
+};
