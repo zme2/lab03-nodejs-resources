88d15006c13b8fed013fdd30b09d40c4feb1d0f6
Batatia, Hadj <hb149@hw.ac.uk>
2021-09-30 21:43:07 +0400

    renamed catalogue

--- apis/routes.js
+++ apis/routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //routes for dynamic processing of products
 //-----------------------------------------------
 //route for listing all products
-router.get('/api/catalog', productController.getCatalog);
+router.get('/api/catalogue', productController.getCatalogue);
 router.get('/api/article/:id', productController.getProductByID);
 
 //export router
